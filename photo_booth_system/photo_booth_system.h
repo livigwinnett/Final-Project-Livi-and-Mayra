c702034d9719fe813b3e466f338b887cb1125aef
livigwinnett <124712098+livigwinnett@users.noreply.github.com>
2023-03-11 14:27:53 -0500

    Create photo_booth_system.h

---
+++ photo_booth_system/photo_booth_system.h
@@ -0,0 +1,19 @@
+//=====[#include guards - begin]===============================================
+
+#ifndef _PHOTO_BOOTH_SYSTEM_H_
+#define _PHOTO_BOOTH_SYSTEM_H_
+
+//=====[Declaration of public defines]=========================================
+
+#define SYSTEM_TIME_INCREMENT_MS   10
+
+//=====[Declaration of public data types]======================================
+
+//=====[Declarations (prototypes) of public functions]=========================
+
+void photoBoothSystemInit();
+void photoBooth();
+
+//=====[#include guards - end]=================================================
+
+#endif // _PHOTO_BOOTH_SYSTEM_H_
