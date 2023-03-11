14b4d4bf8a33c3c6900899ff84b21953758e236e
livigwinnett <124712098+livigwinnett@users.noreply.github.com>
2023-03-11 14:21:33 -0500

    Create coin_slot.h

---
+++ coin_slot/coin_slot.h
@@ -0,0 +1,19 @@
+//=====[#include guards - begin]===============================================
+
+#ifndef _COIN_SLOT_H_
+#define _COIN_SLOT_H_
+
+//=====[Declaration of public defines]=========================================
+
+//=====[Declaration of public data types]======================================
+
+//=====[Declarations (prototypes) of public functions]=========================
+
+bool coinsEntered();
+int count();
+void breakBeamInit();
+void coinsEnteredReset();
+
+//=====[#include guards - end]=================================================
+
+#endif // _COIN_SLOT_H_
